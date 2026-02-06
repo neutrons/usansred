1cccd756692dbb7a6156cb7b944003cedcb7c424
Glass <glass.ships@protonmail.com>
2026-02-06 15:08:10 -0500

    remove unused run number validator

--- src/usansred/model.py
+++ src/usansred/model.py
@@ -32,35 +32,3 @@ class MonitorData:
     xy_data: XYData = field(default_factory=XYData)
     iq_data: IQData = field(default_factory=IQData)
     filepath: str = ""
-
-
-# TODO: finish implementing this (Glass)
-class RunNumberValidator:
-    def validate_run_numbers(run_numbers: str) -> list[str]:
-        """Validate and parse run numbers from a string.
-
-        Valid formats are:
-        - Single run number: "45306"
-        - Comma-separated run numbers: "45306,45307,45308"
-        - Range of run numbers: "45306-45310"
-        - Combination of the above: "45306,45308-45310,45312"
-
-        Parameters
-        ----------
-        run_numbers
-            Comma-separated string of run numbers
-
-        Returns
-        -------
-            List of run numbers as strings
-        """
-        runs = set()
-        parts = run_numbers.split(",")
-        for part in parts:
-            part = part.strip()
-            if "-" in part:
-                start, end = part.split("-")
-                runs.update(str(r) for r in range(int(start), int(end) + 1))
-            else:
-                runs.add(part)
-        return sorted(runs)
